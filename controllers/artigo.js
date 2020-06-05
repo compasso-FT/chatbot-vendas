917bbc68e706ef041c61febfe3810cd8c9f11f21
KarenF <karenf@outlook.com>
2020-06-05 16:27:46 -0300

    update preco

--- controllers/artigo.js
+++ controllers/artigo.js
@@ -9,7 +9,7 @@ module.exports = {
             genero: 'masculino',
             image: 'https://img.lojasrenner.com.br/item/550939291/small/2.jpg',
             url: 'https://www.lojasrenner.com.br/p/mocassim-masculino-em-couro-nobuck/-/A-550939215-br.lr?sku=550939258',
-            preco: '99,90'
+            preco: '99.90'
         },
         {
             id: 550944313,
@@ -20,7 +20,7 @@ module.exports = {
             genero: 'masculino',
             image: 'https://img.lojasrenner.com.br/item/550944381/small/2.jpg',
             url: 'https://www.lojasrenner.com.br/p/sapatenis-masculino-casual-em-couro/-/A-550944292-br.lr?sku=550944313',
-            preco: '139,90'
+            preco: '139.90'
         },
         {
             id: 551210080,
@@ -31,7 +31,7 @@ module.exports = {
             genero: 'feminino',
             image: 'https://img.lojasrenner.com.br/item/551210098/small/2.jpg',
             url: 'https://www.lojasrenner.com.br/p/tenis-feminino-slip-on-detalhe-em-cobra/-/A-551210047-br.lr?sku=551210080',
-            preco: '99,90'
+            preco: '99.90'
         },
         {
             id: 550905429,
@@ -42,7 +42,7 @@ module.exports = {
             genero: 'feminino',
             image: 'https://img.lojasrenner.com.br/item/550905429/small/2.jpg',
             url: 'https://www.lojasrenner.com.br/p/tenis-feminino-com-sola-alta/-/A-550905373-br.lr?sku=550905437',
-            preco: '139,90'
+            preco: '139.90'
         },
         {
             id: 551514454,
@@ -53,7 +53,7 @@ module.exports = {
             genero: 'masculino',
             image: 'https://img.lojasrenner.com.br/item/551514471/small/2.jpg',
             url: 'https://www.lojasrenner.com.br/p/tenis-masculino-sneaker/-/A-551514446-br.lr?sku=551514454',
-            preco: '159,90'
+            preco: '159.90'
         },
         {
             id: 548714631,
@@ -64,7 +64,7 @@ module.exports = {
             genero: 'infantil',
             image: 'https://img.lojasrenner.com.br/item/548714682/small/2.jpg',
             url: 'https://www.lojasrenner.com.br/p/tenis-infantil-abotinado-com-coracoes-tam-28-ao-35/-/A-548714607-br.lr?sku=548714631',
-            preco: '49,90'
+            preco: '49.90'
         },
         {
             id: 551518674,
@@ -75,7 +75,7 @@ module.exports = {
             genero: 'feminino',
             image: 'https://img.lojasrenner.com.br/item/551518711/small/2.jpg',
             url: 'https://www.lojasrenner.com.br/p/tenis-feminino-com-salto-e-brilhos/-/A-551518666-br.lr?sku=551518674',
-            preco: '169,90'
+            preco: '169.90'
         },
         {
             id: 549149056,
@@ -86,7 +86,7 @@ module.exports = {
             genero: 'masculino',
             image: 'https://img.lojasrenner.com.br/item/549149030/small/2.jpg',
             url: 'https://www.lojasrenner.com.br/p/tenis-masculino-slip-on-em-couro-viko/-/A-549149005-br.lr?sku=549149064',
-            preco: '119,90'
+            preco: '119.90'
         },
         {
             id: 548003080,
@@ -97,7 +97,7 @@ module.exports = {
             genero: 'masculino',
             image: 'https://img.lojasrenner.com.br/item/548003055/small/2.jpg',
             url: 'https://www.lojasrenner.com.br/p/tenis-masculino-running-viko/-/A-548003021-br.lr?sku=548003071',
-            preco: '119,90'
+            preco: '119.90'
         },
         {
             id: 550945017,
@@ -108,7 +108,7 @@ module.exports = {
             genero: 'masculino',
             image: 'https://img.lojasrenner.com.br/item/550945033/small/2.jpg',
             url: 'https://www.lojasrenner.com.br/p/tenis-masculino-jogging/-/A-550944962-br.lr?sku=550945017',
-            preco: '119,90'
+            preco: '119.90'
         },
         {
             id: 550947266,
@@ -119,7 +119,7 @@ module.exports = {
             genero: 'masculino',
             image: 'https://img.lojasrenner.com.br/item/550947282/small/2.jpg',
             url: 'https://www.lojasrenner.com.br/p/tenis-masculino-casual-em-couro/-/A-550947207-br.lr?sku=550947266',
-            preco: '119,90'
+            preco: '119.90'
         }]
 
         if (idProd.length > 0) {
